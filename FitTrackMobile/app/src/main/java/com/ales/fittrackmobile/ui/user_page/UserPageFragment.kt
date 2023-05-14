75d0e72dffd678a064f8aafc31854cca950a7959
Alesstintor <77730609+aless-af@users.noreply.github.com>
2023-05-14 15:29:08 +0200

    Workout list logic added

--- FitTrackMobile/app/src/main/java/com/ales/fittrackmobile/ui/user_page/UserPageFragment.kt
+++ FitTrackMobile/app/src/main/java/com/ales/fittrackmobile/ui/user_page/UserPageFragment.kt
@@ -7,7 +7,10 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.ales.fittrackmobile.HomeActivity
 import com.ales.fittrackmobile.databinding.FragmentUserPageBinding
+import com.ales.fittrackmobile.ui.CustomAdapter
 
 class UserPageFragment : Fragment() {
 
@@ -32,6 +35,17 @@ class UserPageFragment : Fragment() {
         userPageViewModel.text.observe(viewLifecycleOwner) {
             username.text = it
         }
+
+        val recyclerView = binding.workoutsList
+
+        recyclerView.setHasFixedSize(true)
+
+        recyclerView.layoutManager = LinearLayoutManager(this.context, LinearLayoutManager.VERTICAL, false)
+
+        val data = Array(10){i -> HomeActivity.Exercise("Title $i", "Subtitle $i")}
+
+        recyclerView.adapter = CustomAdapter(data)
+
         return root
     }
 
