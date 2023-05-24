7e78b9c2d447d81b94f7c1adb66e1a4fbeff401a
Alesstintor <77730609+aless-af@users.noreply.github.com>
2023-05-24 16:21:36 +0200

    Get User and Update User from API

--- FitTrackMobile/app/src/main/java/com/ales/fittrackmobile/LoginActivity.kt
+++ FitTrackMobile/app/src/main/java/com/ales/fittrackmobile/LoginActivity.kt
@@ -3,16 +3,20 @@ package com.ales.fittrackmobile
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ales.fittrackmobile.context.UserContext
 import com.ales.fittrackmobile.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var userContext: UserContext
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        userContext = UserContext.getInstance()
 
         binding.loginButton.setOnClickListener { onLoginButtonClick() }
     }
@@ -20,11 +24,12 @@ class LoginActivity : AppCompatActivity() {
 
     private fun onLoginButtonClick() {
 
-        val username = binding.usernameInput.text.toString()
-        val password = binding.passwordInput.text.toString()
+        //val username = binding.usernameInput.text.toString()
+        //val password = binding.passwordInput.text.toString()
 
         val intent = Intent(this@LoginActivity, HomeActivity::class.java)
 
+        userContext.fetchUserData(this@LoginActivity)
 
         startActivity(intent)
 
